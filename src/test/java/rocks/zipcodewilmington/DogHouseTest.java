3f5f0b6a380296dd19d2370970328fe6c19be236
zekai <zekai@zipcodes-MacBook-Pro.local>
2020-11-06 09:28:42 -0500

    Added test units to satisfy methods

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,8 +1,11 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
 
 import java.util.Date;
@@ -12,13 +15,75 @@ import java.util.Date;
  */
 public class DogHouseTest {
     // TODO - Create tests for `void add(Dog dog)`
-    // TODO - Create tests for `void remove(Integer id)`
-    // TODO - Create tests for `void remove(Dog dog)`
-    // TODO - Create tests for `Dog getDogById(Integer id)`
-    // TODO - Create tests for `Integer getNumberOfDogs()`
-
     @Test
-    public void testGetNumberOfDogs() {
+    public void addByDogTest(){
+        //given
+        Dog dogToBeAdded = new Dog(null, null, 0);
+        DogHouse.add(dogToBeAdded);
+
+        //when
+        Integer id = 0;
+        Dog retrievedDog = DogHouse.getDogById(id);
+
+        // then
+        Assert.assertTrue(retrievedDog instanceof Dog);
+        DogHouse.remove(dogToBeAdded);
+    }
+
+
+    // TODO - Create tests for `void remove(Dog dog)`
+    @Test
+    public void removeByIdTest() {
+        // given
+        Dog dogToBeAdded = new Dog(null, null, 0);
+        DogHouse.add(dogToBeAdded);
+        Integer id = dogToBeAdded.getId();
+
+        // when
+        DogHouse.remove(id);
+        Dog retrievedDog = DogHouse.getDogById(id);
+
+        // then
+        Assert.assertNull(retrievedDog);
+    }
+
+    // TODO - Create tests for `void remove(Dog dog)`
+    @Test
+    public void removeByDogTest() {
+        // given
+        Dog dogToBeAdded = new Dog(null, null, 0);
+        DogHouse.add(dogToBeAdded);
+
+        // when
+        DogHouse.remove(dogToBeAdded);
+        Integer id = dogToBeAdded.getId();
+        Dog retrievedDog = DogHouse.getDogById(id);
+
+        // then
+        Assert.assertNull(retrievedDog);
+    }
+
+    // TODO - Create tests for `Dog getDogById(Integer id)`
+    @Test
+    public void getDogByIdTest() {
+        // given
+        Dog DogToBeAdded = new Dog(null, null, 0);
+        DogHouse.add(DogToBeAdded);
+        Integer Expected = DogToBeAdded.getId();
+
+        // when
+        Integer id = 0;
+        Dog retrievedDog = DogHouse.getDogById(id);
+        Integer Actual = retrievedDog.getId();
+
+        // then
+        Assert.assertEquals(Expected,Actual);
+        DogHouse.remove(DogToBeAdded);
+    }
+
+    // TODO - Create tests for `Integer getNumberOfDogs()`
+    @Test
+    public void GetNumberOfDogsTest() {
         // Given (some
         String name = "Milo";
         Date birthDate = new Date();
